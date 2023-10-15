38b865d84eb4cd5bde5076022ad055f0ae0b59f7
Vahram Alickhanyan <anton-sviridov92@mail.ru>
2023-10-15 22:50:11 +0300

    Homework

---
+++ src/Main.java
@@ -0,0 +1,149 @@
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+public class Main {
+    private static int[] nums = new int[]{0, 0, 100};
+    private static int target = 100;
+
+    public static void main(String[] args) {
+        Main main = new Main();
+        System.out.println(Arrays.toString(main.twoSum(nums, target)));
+        System.out.println(main.containsDuplicate(nums));
+    }
+
+    public int[] twoSum(int[] nums, int target) {
+        for (int i = 0; i < nums.length - 1; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[]{i, j};
+                }
+            }
+        }
+        return null;
+    }
+
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        for (int num : nums) {
+            if (set.contains(num)) {
+                return true;
+            }
+            set.add(num);
+        }
+        return false;
+    }
+}
+//        ArrayList<String> arrayList1 = new ArrayList<>();
+//        arrayList1.add("Maksim");
+//        arrayList1.add("Pavel");
+//        arrayList1.add("Ignat");
+//        arrayList1.add(1, "Vahram");
+//
+//        System.out.println(arrayList1);
+//
+//        ArrayList<String> arrayList2 = new ArrayList<>();
+//        arrayList2.add("Yulia");
+//        arrayList2.add("Ludmila");
+//        arrayList2.add("Natasha");
+//
+//        System.out.println(arrayList2);
+//
+//        ArrayList<String> arrayList3 = new ArrayList<>(arrayList2);
+//        System.out.println(arrayList3);
+//        System.out.println(arrayList1 == arrayList3);
+//
+//        for (String s : arrayList1) {
+//            System.out.print(s + " ");
+//
+//        }
+//        System.out.println();
+//        for (int i = 0; i < arrayList1.size(); i++) {
+//            System.out.println(arrayList1.get(i));
+//        }
+//        arrayList2.set(1, "Milana");
+//        arrayList1.remove(2);
+//        System.out.println(arrayList2);
+//        System.out.println(arrayList1);
+//    }
+//}
+//    LinkedList<String> ll = new LinkedList<String>();
+//    ll.add("F");
+//    ll.add("B");
+//    ll.add("D");
+//    ll.add("E");
+//    ll.add("C");
+//    ll.addLast("Z");
+//    ll.addFirst("A");
+//    ll.add(1, "A2");
+//    System.out.println("Исходное содержимое связного списка ll: " + ll);
+//    ll.remove("F");
+//    ll.remove(2);
+//    System.out.println("Исходное содержимое связного списка ll: " + ll);
+//    ll.removeFirst();
+//    ll.removeLast();
+//    System.out.println("Исходное содержимое связного списка ll: " + ll);
+//    String val = ll.get(2);
+//    ll.set(2, val + " изменено");
+//    System.out.println("Содержимое связного списка ll после изменения: " + ll);
+//    }
+//}
+//    ArrayList<Integer> al = new ArrayList<Integer>();
+//    al.add(1);
+//    al.add(2);
+//    al.add(3);
+//    al.add(4);
+//    System.out.println("Содержимое списочного массива al: " + al);
+//    Integer ia[] = new Integer[al.size()];
+//    ia = al.toArray(ia);
+//    int sum = 0;
+//    for (int i : ia) sum += i;
+//    System.out.println("Сумма: " + sum);
+//    }
+//}
+//        ArrayList <String> al = new ArrayList<String>();
+//        System.out.println("Начальный размер списочного массива al: " + al.size());
+//        al.add("C");
+//        al.add("A");
+//        al.add("E");
+//        al.add("B");
+//        al.add("D");
+//        al.add("F");
+//        al.add(1, "A2");
+//        System.out.println("размер списочного массива al после ввода элементов: " + al.size());
+//
+//        System.out.println("Содержимое списочного массива al " + al);
+//
+//        al.remove("F");
+//        al.remove(2);
+//
+//        System.out.println("Размер списочного массива al после удаления элементов: " + al.size());
+//        System.out.println("Содержимое списочного массива al " + al);
+//        }
+//    }
+
+//public class Main {
+//    public static void main(String[] args, UnicodeSet list) {
+//        System.out.println("Hello world!");
+//        ArrayList List = new ArrayList<>();
+//        int[] ArrayList = new int[4];
+//        ArrayList[0] = 3;
+//        ArrayList[1] = 8;
+//        ArrayList[2] = 15;
+//        ArrayList[3] = 17;
+//        System.out.println(ArrayList[1, 2]);
+
+//        ArrayList List = new ArrayList<>();
+//            list.add("a");
+//            list.add("b");
+//            list.add("c");
+//            list.add("b");
+//            list.add("d");
+//            list.add("d");
+//            list.add("e");
+//        System.out.println(list);
+//
+//        while(list.remove("c" + "d"));
+//        System.out.println(list);
+//
+//    }
+//}
---
+++ src/Student/Student.java
@@ -0,0 +1,59 @@
+package Student;
+
+//public class Student {
+//    public static void main(String[] args) {
+//
+//        Student st1 = new Student("Ivan", 'm', 22, 3, 8.3);
+//        Student st2 = new Student("Nadya", 'f', 21, 2, 8.4);
+//        Student st3 = new Student("Pavel", 'm', 23, 4, 7.5);
+//        Student st4 = new Student("Natasha", 'f', 25, 5, 8.6);
+//        Student st5 = new Student("Ignat", 'm', 19, 3, 9.7);
+//        Student st6 = new Student("Yulia", 'f', 28, 2, 9.9);
+//        Student st7 = new Student("Denis", 'm', 20, 1, 7.5);
+//
+//        ArrayList<Student> studentList = new ArrayList<>();
+//        studentList.add(st1);
+//        studentList.add(st2);
+//        studentList.add(st3);
+//        studentList.add(st4);
+//        studentList.add(st5);
+//        studentList.add(st6);
+//        studentList.add(st7);
+//
+//        System.out.println(studentList);
+//        Student st8 = new Student("Yulia", 'f', 28, 2, 9.9);
+//        studentList.remove(st6);
+//        System.out.println(studentList);
+//
+//        ArrayList<String> arrayList4 = new ArrayList<>();
+//        arrayList4.add("Vlad");
+//        arrayList4.add("Stepan");
+//    }
+//
+//    private String name;
+//    private char sex;
+//    private int age;
+//    private int course;
+//    private double avgGrade;
+//
+//    public Student(String name, char sex, int age, int course, double avgGrade) {
+//        this.name = name;
+//        this.sex = sex;
+//        this.age = age;
+//        this.course = course;
+//        this.avgGrade = avgGrade;
+//    }
+//
+//    @Override
+//    public String toString() {
+//        return "Student {" +
+//                "name'" + name + '\'' +
+//                ", sex=" + sex +
+//                ", age=" + age +
+//                ", course=" + course +
+//                ", avgGrage=" + avgGrade +
+//                '}';
+//    }
+//}
+
+
